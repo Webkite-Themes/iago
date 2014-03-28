75ae1901dbe5c7b0232d5989316e2bec57480393
Greg Gates <gregates@gmail.com>
2014-03-28 17:15:11 -0400

    cleanup

--- routes/index.js
+++ routes/index.js
@@ -1,7 +1,5 @@
 
-var path = require('path'),
-    fs = require('fs'),
-    rsvp = require('rsvp'),
+var fs = require('fs'),
     _ = require('lodash');
 
 /*
@@ -12,11 +10,9 @@ exports.index = function(req, res){
   fs.readdir(process.cwd(), function(err, files) {
     if (err) { throw err; }
     files = _(files).reject(function(file) {
-      console.log(file);
       var stats = fs.statSync(file),
           reject = false;
       reject = (stats.isDirectory() ? false : true);
-      console.log(stats.isDirectory());
       reject = reject || _(['node_modules', 'public', 'routes', 'views', '.git']).contains(file);
       return reject;
     }).value();
