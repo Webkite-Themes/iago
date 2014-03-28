89fea1805cef16ed223f2122fa2fdfc761b4be77
Greg Gates <gregates@gmail.com>
2014-03-28 17:05:39 -0400

    List themes in index

--- routes/index.js
+++ routes/index.js
@@ -1,8 +1,26 @@
 
+var path = require('path'),
+    fs = require('fs'),
+    rsvp = require('rsvp'),
+    _ = require('lodash');
+
 /*
  * GET home page.
  */
 
 exports.index = function(req, res){
-  res.render('index', { title: 'Iago (WebKite)' });
+  fs.readdir(process.cwd(), function(err, files) {
+    if (err) { throw err; }
+    files = _(files).reject(function(file) {
+      console.log(file);
+      var stats = fs.statSync(file),
+          reject = false;
+      reject = (stats.isDirectory() ? false : true);
+      console.log(stats.isDirectory());
+      reject = reject || _(['node_modules', 'public', 'routes', 'views', '.git']).contains(file);
+      return reject;
+    }).value();
+
+    res.render('index', { title: 'Iago (WebKite)', themes: files });
+  });
 };
