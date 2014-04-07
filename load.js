cb210767a0f1f2478d2c3c76b772f68539210065
Greg Gates <gregates@gmail.com>
2014-04-07 12:03:30 -0400

    Use platform-agnostic file path constructor

--- load.js
+++ load.js
@@ -1,15 +1,24 @@
+'use strict';
+
 var fs = require('fs'),
+    path = require('path'),
     _ = require('lodash');
+
+var folders,
+    themes,
+    config;
 
 folders = fs.readdirSync(process.cwd());
 
 themes = _(folders).reject(function(file) {
   var stats = fs.statSync(file),
+      isTheme = false,
       reject = false;
   reject = (stats.isDirectory() ? false : true);
-  reject = reject || !fs.existsSync([file, 'manifest.yml'].join('/'));
-  return reject;
+  return reject || !fs.existsSync(path.join(file, 'manifest.yml'));
 }).value();
+
+//config = fs.readFileSync();
 
 module.exports = {
   themes: themes
