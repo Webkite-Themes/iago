ddb79a63bd6bfd489758db9cb940b4cb9656a754
Steve McCarthy <steve@webkite.com>
2014-05-06 12:26:37 -0400

    Return 409 if creating a use case that already exists

--- routes/useCases.js
+++ routes/useCases.js
@@ -7,8 +7,13 @@ exports.index = function(req, res){
 };
 
 exports.create = function(req, res){
-  h.overwriteConfigWithPath(req.body, app.config().configPath, req.body.name + '.json');
-  app.useCases.push(req.body);
-  res.json(app.useCases);
+  // TODO: do this check in ember, before hitting the route
+  if (h.loadConfigWithPathConverted(app.config().configPath, req.body.name + '.json')) {
+      res.send(409, 'Conflict: Use Case Already Exists');
+  } else {
+    h.overwriteConfigWithPath(req.body, app.config().configPath, req.body.name + '.json');
+    app.useCases.push(req.body);
+    res.json(app.useCases);
+  }
 };
 
