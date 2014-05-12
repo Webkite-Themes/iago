f2bdf8045dcfe480cdd9fb78210d68399fea0f39
Steve McCarthy <steve@webkite.com>
2014-05-12 18:38:04 -0400

    Add get route to retrieve use case by name
    Clarify updating use case parameter, which uses the use case name.

--- routes/useCases.js
+++ routes/useCases.js
@@ -17,11 +17,24 @@ exports.create = function(req, res){
   }
 };
 
+// param: useCaseName
+exports.get = function(req, res){
+  var useCase = app.useCases.filter(function(useCase) {
+    return useCase.name.toLowerCase() === req.params.useCaseName.toLowerCase();
+  });
+  if (useCase.length === 1) {
+    res.json(useCase[0]);
+  } else {
+    res.send(404, "Use Case not found");
+  }
+};
+
+// param: useCaseName
 exports.update = function(req, res){
-  if (req.params.useCase != req.body.name) {
+  if (req.params.useCaseName != req.body.name) {
     res.send(409, 'Conflict: Use Case does not match Use Case\'s Name');
   } else {
-    var useCaseConfig = h.loadConfigWithPathConverted(app.config().configPath, req.params.useCase + '.json');
+    var useCaseConfig = h.loadConfigWithPathConverted(app.config().configPath, req.params.useCaseName + '.json');
     if (useCaseConfig) {
       var updatedConfig = h.saveConfigWithPath(req.body, useCaseConfig, app.config().configPath, req.body.name + '.json');
       res.json(updatedConfig);
