3d9261a6d5f6c5c63763e9cae6b9a928ea99d8ee
Steve McCarthy <steve@webkite.com>
2014-05-14 15:47:44 -0400

    Start model helpers for client-side data storage

---
+++ public/javascripts/models/helpers.js
@@ -0,0 +1,91 @@
+// Use Case Model
+var UseCaseModel = function() { this.value = []; };
+
+UseCaseModel.prototype.create = function(properties) {
+  return Iago.UseCase.create({
+    name: properties.name,
+    description: properties.description,
+    icon: properties.icon,
+    spreadsheetKey: properties.spreadsheetKey,
+    datasetUuid: properties.datasetUuid,
+    themes: properties.themes
+  });
+};
+
+UseCaseModel.prototype.all = function() {
+  return this.value;
+};
+
+UseCaseModel.prototype.find = function(useCaseName) {
+  return _(this.value).find(function(useCase) {
+    return useCase.get('name') === useCaseName;
+  });
+};
+
+UseCaseModel.prototype.push = function(useCase) {
+  if (this.find(useCase.get('name'))) {
+    return false;
+  } else {
+    this.value.push(useCase);
+    return true;
+  }
+};
+
+UseCaseModel.prototype.update = function(properties) {
+};
+
+UseCaseModel.prototype.save = function() {
+};
+
+// Theme Model
+var ThemeModel = function() { this.value = []; };
+
+ThemeModel.prototype.create = function(properties) {
+  return Iago.Theme.create({
+    name: properties.name,
+    org: properties.org,
+    version: properties.version,
+    useCaseName: properties.useCaseName
+  });
+};
+
+ThemeModel.prototype.all = function() {
+  return this.value;
+};
+
+ThemeModel.prototype.find = function(themeName) {
+  return _(this.value).find(function(theme) {
+    return theme.get('name') === themeName;
+  });
+};
+
+ThemeModel.prototype.findAll = function(themeNames) {
+  if (!themeNames)
+    return [];
+  return _(this.value).filter(function(theme) {
+    return themeNames.indexOf(theme.get('name')) >= 0;
+  }).value();
+};
+
+ThemeModel.prototype.push = function(theme) {
+  if (!theme.get('useCaseName')) {
+    return false;
+  } else {
+    if (this.find(theme.get('themeName'))) {
+      return false;
+    } else {
+      this.value.push(theme);
+      return true;
+    }
+  }
+};
+
+ThemeModel.prototype.update = function(properties) {
+};
+
+ThemeModel.prototype.save = function() {
+};
+
+// Instantiate
+Iago.UseCaseModel = new UseCaseModel();
+Iago.ThemeModel = new ThemeModel();
